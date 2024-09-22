0dea999fd84e31cc884a24f255a7b0df8966cf54
risetabajoo <amirarabpurwakarta@gmail.com>
2024-09-22 13:26:44 +0700

    Create script.js

---
+++ agregate/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const animatedTexts = document.querySelectorAll(".animated-text");
+    let delay = 0;
+
+    animatedTexts.forEach((elem) => {
+        setTimeout(() => {
+            elem.classList.add("show");
+            // For paragraphs, show the entire text at once
+            if (elem.tagName.toLowerCase() === "p") {
+                elem.textContent = elem.getAttribute("data-text");
+            }
+            // For list items, show the text
+            if (elem.tagName.toLowerCase() === "li") {
+                elem.textContent = elem.getAttribute("data-text");
+            }
+        }, delay);
+        delay += 500; // Delay 0.5 detik antara setiap elemen
+    });
+});
