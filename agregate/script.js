3c54542209f81eeaeb4d569f56de4da9b221fad8
risetabajoo <amirarabpurwakarta@gmail.com>
2024-09-22 13:37:52 +0700

    Update script.js

--- agregate/script.js
+++ agregate/script.js
@@ -14,6 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 elem.textContent = elem.getAttribute("data-text");
             }
         }, delay);
-        delay += 1000; // Delay 0.5 detik antara setiap elemen
+        delay += 500; // Delay 0.5 detik antara setiap elemen
     });
 });
